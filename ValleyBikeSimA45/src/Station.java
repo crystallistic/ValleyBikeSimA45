444d72ef5da314b94e48f90259deee30d9c00894
Mai Ngo <mtngo@smith.edu>
2019-11-21 16:51:13 -0500

    added setters and getters for Station class

--- ValleyBikeSimA45/src/Station.java
+++ ValleyBikeSimA45/src/Station.java
@@ -6,7 +6,9 @@
  * @author maingo
  *
  */
+
 public class Station {
+	
 	
 	int stationId;
 	String address;
@@ -16,48 +18,91 @@ public class Station {
 	int maintenanceReqs;
 	boolean hasKiosk;
 	
+	
+	/**
+	 * Getter for stationId
+	 * @return the station's id number
+	 */
 	public int getStationId() {
 		return stationId;
 	}
+	/**
+	 * Sets the station ID to the given value
+	 * @param stationId The station's id number
+	 */
 	public void setStationId(int stationId) {
 		this.stationId = stationId;
 	}
+	/**
+	 * @return
+	 */
 	public String getAddress() {
 		return address;
 	}
+	/**
+	 * @param address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	/**
+	 * @return
+	 */
 	public int getNumBikes() {
 		return numBikes;
 	}
+	/**
+	 * @param numBikes
+	 */
 	public void setNumBikes(int numBikes) {
 		this.numBikes = numBikes;
 	}
+	/**
+	 * @return
+	 */
 	public int getNumFreeDocks() {
 		return numFreeDocks;
 	}
+	/**
+	 * @param numFreeDocks
+	 */
 	public void setNumFreeDocks(int numFreeDocks) {
 		this.numFreeDocks = numFreeDocks;
 	}
+	/**
+	 * @return
+	 */
 	public int getCapacity() {
 		return capacity;
 	}
+	/**
+	 * @param capacity
+	 */
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
+	/**
+	 * @return
+	 */
 	public int getMaintenanceReqs() {
 		return maintenanceReqs;
 	}
+	/**
+	 * @param maintenanceReqs
+	 */
 	public void setMaintenanceReqs(int maintenanceReqs) {
 		this.maintenanceReqs = maintenanceReqs;
 	}
+	/**
+	 * @return
+	 */
 	public boolean isHasKiosk() {
 		return hasKiosk;
 	}
+	/**
+	 * @param hasKiosk
+	 */
 	public void setHasKiosk(boolean hasKiosk) {
 		this.hasKiosk = hasKiosk;
 	}
-	
-	
 }
