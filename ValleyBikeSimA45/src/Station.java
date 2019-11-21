7c1bd9f8a212bb9988d41b9434cbae7bb055652f
Mai Ngo <mtngo@smith.edu>
2019-11-21 11:54:05 -0500

    station class

--- ValleyBikeSimA45/src/Station.java
+++ ValleyBikeSimA45/src/Station.java
@@ -7,5 +7,57 @@
  *
  */
 public class Station {
-	// add stuff here later
+	
+	int stationId;
+	String address;
+	int numBikes;
+	int numFreeDocks;
+	int capacity;
+	int maintenanceReqs;
+	boolean hasKiosk;
+	
+	public int getStationId() {
+		return stationId;
+	}
+	public void setStationId(int stationId) {
+		this.stationId = stationId;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getNumBikes() {
+		return numBikes;
+	}
+	public void setNumBikes(int numBikes) {
+		this.numBikes = numBikes;
+	}
+	public int getNumFreeDocks() {
+		return numFreeDocks;
+	}
+	public void setNumFreeDocks(int numFreeDocks) {
+		this.numFreeDocks = numFreeDocks;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public int getMaintenanceReqs() {
+		return maintenanceReqs;
+	}
+	public void setMaintenanceReqs(int maintenanceReqs) {
+		this.maintenanceReqs = maintenanceReqs;
+	}
+	public boolean isHasKiosk() {
+		return hasKiosk;
+	}
+	public void setHasKiosk(boolean hasKiosk) {
+		this.hasKiosk = hasKiosk;
+	}
+	
+	
 }
